2eabfeede178bc4ff1f873a5b1d39195a2ab2a46
Lady-Bee <bisimi16@gmail.com>
2023-10-06 13:58:39 -0700

    triple number combo

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - this prints different combinations of three digits
+ *
+ * Return: returns 0
+ */
+
+int main(void)
+{
+	int num1;
+	int num2;
+	int num3;
+
+	for (num3 = '0'; num3 <= '9'; num3++)
+	{
+		for (num2 = '0'; num2 <= '9'; num2++)
+		{
+			for (num1 = '0'; num1 <= '9'; num1++)
+			{
+				if (!((num1 == num2) || (num1 == num3) ||
+(num2 > num1) || (num3 > num2)))
+				{
+					putchar(num3);
+					putchar(num2);
+					putchar(num1);
+					if (!(num1 == '9' && num3 == '7' && num2 == '8'))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
