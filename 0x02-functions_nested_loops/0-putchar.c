98cba37d42fa6554fc95e01704e09603081fdfd1
Lady-Bee <bisimi16@gmail.com>
2023-10-10 02:56:45 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - This program prints putchar followed by a new line
+ *
+ * Return: returns 0 when successful
+ */
+
+int main(void)
+{
+	int i = 2;
+
+	putchar( "_putchar");
+	putchar('\n');
+	return (0);
+}
