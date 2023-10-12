a0b918206003b5e2e64536aa7182e2e686ce9686
Lady-Bee <bisimi16@gmail.com>
2023-10-12 04:50:07 -0700

    multiply

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - This program prints putchar followed by a new line
+ *@a: first parameter to be checked
+ *@b: second parameter to be checked
+ * Return: returns 0 when successful
+ */
+
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+		return (result);
+}
