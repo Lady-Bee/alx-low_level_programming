f4d1a8f3b43916cda5f43058a389bc421b14a439
Lady-Bee <bisimi16@gmail.com>
2023-10-12 07:17:32 -0700

    print 0-9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - This program prints from 0 -9 followed by a new line
+ *
+ * Return: returns 0 when successful
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = '48'; i <= '58'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
