222eb63390d7c59af8f82c0e93b736461d0896d1
Lady-Bee <bisimi16@gmail.com>
2023-10-18 04:13:52 -0700

    comparing strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,10 +4,22 @@
  * _strcmp - this compares two strings
  * @s1: first string value
  * @s2: second string value
- * Return: this is void
+ * Return: 0
  */
 
 int _strcmp(char *s1, char *s2)
 {
-        
+	int a;
+
+	for (a = 0 ; s1[a] != '\0' || s2[a] != '\0' ; a++)
+	{
+		if (s1[a] != s2[a])
+		{
+			if (s2[a] > s1[a])
+				return (s1[a] - s2[a]);
+			else if (s1[a] > s2[a])
+				return (s1[a] - s2[a]);
+		}
+	}
+	return (0);
 }
