0ddcb74f36b49787dbcceaa3b9f9a82d66536545
Lady-Bee <bisimi16@gmail.com>
2023-10-18 05:32:40 -0700

    string capitalization

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,11 +2,37 @@
 
 /**
  * cap_string - function capitalizes all words of a string
- *  
- * Return: this is void
+ * @str: parameter of the string to be capitalized
+ * Return: str
  */
 
-char *cap_string(char *)
+char *cap_string(char *str)
 {
-        
+	int position = 0;
+
+	while (str[position])
+	{
+		while (!(str[position] >= 'a' && str[position] <= 'z'))
+		position++;
+
+	if (str[position - 1] == ' ' ||
+	str[position - 1] == '\t' ||
+	str[position - 1] == '\n' ||
+	str[position - 1] == ',' ||
+	str[position - 1] == ';' ||
+	str[position - 1] == '.' ||
+	str[position - 1] == '!' ||
+	str[position - 1] == '?' ||
+	str[position - 1] == '"' ||
+	str[position - 1] == '(' ||
+	str[position - 1] == ')' ||
+	str[position - 1] == '{' ||
+	str[position - 1] == '}' ||
+	position == 0)
+		str[position] -= 32;
+	position++;
+	}
+	return (str);
 }
+
+
