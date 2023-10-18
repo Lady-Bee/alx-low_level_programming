ee3d5e2ce2d72bbd6c8c4e57cdddaf53f341b817
Lady-Bee <bisimi16@gmail.com>
2023-10-18 05:18:15 -0700

    string_lower to upper case

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -2,11 +2,19 @@
 
 /**
  * string_toupper - changes a string's lowercase letters to uppercase
- *  
- * Return: this is void
+ * @i: this ia a pointer
+ * Return: returns i
  */
 
-char *string_toupper(char *)
+char *string_toupper(char *i)
 {
-        
+	int a;
+
+	for (a = 0 ; i[a] != '\0' ; )
+	{
+		if (i[a] >= 'a' && i[a] <= 'z')
+			i[a] = i[a] - 32;
+		a++;
+	}
+	return (i);
 }
