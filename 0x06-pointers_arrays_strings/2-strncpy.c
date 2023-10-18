226f197ed292721b250ab0c192c1bebc014b2092
Lady-Bee <bisimi16@gmail.com>
2023-10-18 13:41:04 -0700

    copying a string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -5,13 +5,13 @@
  * @dest: the destination string
  * @src: the source string
  * @n: number of bytes to be used
- * Return: this is void
+ * Return: dest
  */
 
 char *_strncpy(char *dest, char *src, int n)
 {
 	int a;
-	
+
 	a = 0;
 
 	while (a < n && src[a] != '\0')
