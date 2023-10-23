dc7ca6815bee7ba5c299bcdb92a7ff1e544521b2
Lady-Bee <bisimi16@gmail.com>
2023-10-23 13:40:21 -0700

    double pointer

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -2,12 +2,12 @@
 
 /**
  * set_string - this function sets the value of a pointer to a char
- * @s: input
- * @to: input
+ * @s: pointer
+ * @to: char
  * Return: returns 0 when successfsul
  */
 
 void set_string(char **s, char *to)
 {
-
+	*s = to;
 }
