93685b5e14fc042c0ae7f40de6163bec5068e1ff
Lady-Bee <bisimi16@gmail.com>
2023-10-23 12:59:44 -0700

    task 4

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _strpbrk - this function locates the first occurence in a string 
+ * _strpbrk - this function locates the first occurence in a string
  * @s: input
  * @accept: input
  * Return: returns 0 when successfsul
@@ -9,5 +9,17 @@
 
 char *_strpbrk(char *s, char *accept)
 {
+	int i;
 
+	while (*s)
+	{
+		for (i = 0; accept[i]; i++)
+		{
+		if (*s == accept[i])
+		return (s);
+		}
+	s++;
+	}
+	return ('\0');
 }
+
