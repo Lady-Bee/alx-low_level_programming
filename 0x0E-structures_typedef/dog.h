5d51e57e7e5ef8a2d0c036f46b76e550acdef7ef
Lady-Bee <bisimi16@gmail.com>
2023-11-06 10:39:52 -0800

    defining a new struct type

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,19 @@
-#include <stdio.h>
+#ifndef DOG_H
+#define DOG_H
 
+/**
+ * struct dog - this is the dog structure
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the owner's name
+ */
 
-struct dog 
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
 typedef struct dog dog;
+
+#endif
