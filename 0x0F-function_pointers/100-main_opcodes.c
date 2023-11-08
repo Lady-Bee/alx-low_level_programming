a2f8acaf50aba60f3b34a2cc1c980721c4c791fb
Lady-Bee <bisimi16@gmail.com>
2023-11-08 08:45:22 -0800

    opcodes

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,42 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+
+/**
+ * main - prints its function opcodes
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: returns 0 if successful
+ */
+
+int main(int argc, char *argv[])
+{
+	int bytes, i;
+	char *array;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	bytes = atoi(argv[1]);
+
+	if (bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	array = (char *)main;
+
+	for (i = 0; i < bytes; i++)
+	{
+		if (i == bytes - 1)
+		{
+			printf("%02hhx\n", array[i]);
+			break;
+		}
+		printf("%02hhx ", array[i]);
+	}
+	return (0);
+}
