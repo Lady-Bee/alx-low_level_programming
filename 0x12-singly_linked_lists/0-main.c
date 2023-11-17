3626243ac18a2e04a62be7fc5e7a472a40c2621d
Lady-Bee <bisimi16@gmail.com>
2023-11-16 16:09:51 -0800

    print list

--- 0x12-singly_linked_lists/0-main.c
+++ 0x12-singly_linked_lists/0-main.c
@@ -14,10 +14,10 @@ int main(void)
 	list_t *new;
 	list_t hello = {"World", 5, NULL};
 	size_t n;
-	
+
 	head = &hello;
 	new = malloc(sizeof(list_t));
-	
+
 	if (new == NULL)
 	{
 		printf("Error\n");
@@ -29,13 +29,13 @@ int main(void)
 	head = new;
 	n = print_list(head);
 	printf("-> %lu elements\n", n);
-	
+
 	printf("\n");
 	free(new->str);
 	new->str = NULL;
 	n = print_list(head);
 	printf("-> %lu elements\n", n);
-	
+
 	free(new);
 	return (0);
 }
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -3,14 +3,13 @@
 
 #include <stddef.h>
 
-
+/*This is the structure for a singly linked list node*/
 typedef struct list_s
 {
 	char *str;
 	unsigned int len;
 	struct list_s *next;
 } list_t;
-
 
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
