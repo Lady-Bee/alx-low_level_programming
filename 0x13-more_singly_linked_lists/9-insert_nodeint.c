1047892e2381cdc1c676dc96ec55f95f73fb6491
Lady-Bee <bisimi16@gmail.com>
2023-11-21 08:07:07 -0800

    inserting new node at a given position

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -50,6 +50,5 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	new_node->n = n;
 	new_node->next = current->next;
 	current->next = new_node;
-
 	return (new_node);
 }
