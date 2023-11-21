84a86726b01f8b24df32133fd83e740cbe794549
Lady-Bee <bisimi16@gmail.com>
2023-11-21 06:07:17 -0800

    freeing a list 2

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,11 +8,10 @@
 void free_listint2(listint_t **head)
 {
 	listint_t *temp;
-	*head = NULL;
 
 	if (head == NULL)
 		return;
-	while (head)
+	while (*head)
 	{
 		temp = *head;
 		*head = temp->next;
