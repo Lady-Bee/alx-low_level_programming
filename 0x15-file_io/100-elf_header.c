fb6077ba582f6c2818cb5624e148618000a26c60
Lady-Bee <bisimi16@gmail.com>
2023-11-30 21:46:25 -0800

    elf-header

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - Program entry point
+ * @argc: The number of arguments
+ * @argv: arguments array pointer
+ * Return: 1 if successful, error code on failure
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("argc:%d, argv:%p\n", argc, (void *)*argv);
+	return (1);
+}
