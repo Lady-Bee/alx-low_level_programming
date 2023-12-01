a49532a243e57e5e5ee312d2907328a5ea92fa50
Lady-Bee <bisimi16@gmail.com>
2023-11-30 20:51:28 -0800

    create_file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 
-/*
+/**
  * create_file - function creates a file
  * @filename: name of file to create
  * @text_content: NULL terminated string to write to the file
@@ -10,5 +10,21 @@
 
 int create_file(const char *filename, char *text_content)
 {
+	int file_do, file_dw, len = 0;
 
+	if (filename == NULL)
+		return (-1);
+
+	file_do = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	if (file_do < 0)
+		return (-1);
+
+	while (text_content && *(text_content + len))
+		len++;
+
+	file_dw = write(file_do, text_content, len);
+	close(file_do);
+	if (file_dw < 0)
+		return (-1);
+	return (1);
 }
